91f91e82a94ed7052906a2381b4f064c51514d7e
Christopher Bailey <cbailey@mort.is>
2022-05-03 12:15:24 -0400

    Updates template with auto-minify support

--- .bin/manifest.sh
+++ .bin/manifest.sh
@@ -5,9 +5,18 @@ if [[ -f deps.json ]]; then
     DEPS=$(jq -c < deps.json)
 fi
 
-find . -iname "*.lua" \
-    | sed 's/.\///' \
+LUA_FILES=$(find . -iname "*.lua" ! -iname '*.min.lua' | sed 's/.\///')
+echo "All files:"
+echo "$LUA_FILES"
+echo "Generating manifest.json..."
+echo $LUA_FILES \
     | xargs sha256sum \
     | awk '{ print "{\""$2"\": \""$1"\"}" }' \
     | jq -s add \
     | jq --argjson dependencies $DEPS '{files: ., dependencies: $dependencies}' > manifest.json
+echo "Minifying Lua files..."
+for file in $LUA_FILES; do
+    minFile=${file%.lua}.min.lua
+    echo "$file -> $minFile"
+    cat "$file" | luamin -c > $minFile
+done
